1ac6b64fbb8e1475b1359368120bdc88e1ed2ffa
IGhos-P <dndb3599@gmail.com>
2022-06-22 09:58:03 +0900

    init : react-native 설치 완료

---
+++ YAMYAMAPP/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>YAMYAMY!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
