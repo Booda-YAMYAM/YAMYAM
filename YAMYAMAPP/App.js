f5073c5eb050e70e0eab4c4ac92d9710b463a0f6
IGhos-P <dndb3599@gmail.com>
2022-06-22 10:32:01 +0900

    init : react-stack navigator 설치 완료

--- YAMYAMAPP/App.js
+++ YAMYAMAPP/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+//App.js
 
-const App = () => {
+import React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import HomeScreen from './screens/HomeScreen';
+// import DetailScreen from './screens/DetailScreen';
+
+const Stack = createNativeStackNavigator(); // 뒤로가기 앞으로 가기를 위한, 스택
+
+const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>YAMYAMY!</Text>
-      </View>
-    </SafeAreaView>
+    <View>
+      <Text>Home</Text>
+    </View>
   );
 };
 
+const DetailScreen = () => {
+  return (
+    <View>
+      <Text>Detail</Text>
+    </View>
+  );
+};
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 export default App;
